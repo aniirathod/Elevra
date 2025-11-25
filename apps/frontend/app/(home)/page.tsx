c3620dd1b932c4b3b69281f1ab2de0d12d06de33
aniirathod <aniketnr5023@gmail.com>
2025-11-25 11:53:46 +0000

    chore:testing frontend deployment

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div className="bg-black text-white min-h-screen ">Home page </div>;
+  return <div className="bg-black text-white min-h-screen text-3xl">Home page </div>;
 };
 
 export default Home;
