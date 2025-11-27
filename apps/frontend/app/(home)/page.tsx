5cd92c1c53e35b33bdc11be54c387346b8fd3070
aniirathod <aniketnr5023@gmail.com>
2025-11-27 17:00:36 +0000

    test: frontend change

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  console.log('Home page rendered');
-  return <div className="bg-black text-orange-700 min-h-screen text-3xl ">Home page </div>;
+  return <div className="bg-blue-600 text-orange-700 min-h-screen text-3xl ">Home page </div>;
 };
 
 export default Home;
