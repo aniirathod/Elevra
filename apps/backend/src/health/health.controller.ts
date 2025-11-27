57fa4b009beea6103fbb41bfb024fe760a636e71
aniirathod <aniketnr5023@gmail.com>
2025-11-27 17:14:35 +0000

    testing

--- apps/backend/src/health/health.controller.ts
+++ apps/backend/src/health/health.controller.ts
@@ -30,7 +30,7 @@ export class HealthController {
     } catch (err) {
       return {
         status: 'error',
-        message: `Database connection fail ${err}`,
+        message: `Database connection failed ${err}`,
         timestamp: new Date().toISOString(),
         service: 'elevra-backend',
         uptime: process.uptime(),
--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div className="bg-blue-600 text-orange-700 min-h-screen text-3xl ">Home page </div>;
+  return <div className="bg-black text-orange-700 min-h-screen text-3xl ">Home page </div>;
 };
 
 export default Home;
