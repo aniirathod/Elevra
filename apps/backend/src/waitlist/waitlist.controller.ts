d4a7da2c543cc98cf28d7f5af1c968c1ec9c2025
aniirathod <aniketnr5023@gmail.com>
2025-11-30 20:36:14 +0000

    chore:remove unused import

--- apps/backend/src/waitlist/waitlist.controller.ts
+++ apps/backend/src/waitlist/waitlist.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { WaitlistService } from './waitlist.service';
 import { WaitlistDto } from './dto/waitlist.dto';
 
